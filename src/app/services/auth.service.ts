2b09d2bd960109b475715ea9a4bf644bde04d471
sidney_silva_verity <jhonyxf@gmail.com>
2021-09-10 16:00:11 -0300

    Criação do serviço Auth

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private userSubject: BehaviorSubject<User>;
+  public user!: Observable<User>;
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.userSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser') || '{}')
+    );
+
+    this.user = this.userSubject.asObservable();
+  }
+
+  public get userValue(): User{
+    return this.userSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<any>(`${environment.apiUrl}/api/login`, { username, password })
+      .pipe(
+        map(({token}) => {
+          let user: User = {
+            email: username,
+            token: token,
+          };
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.userSubject.next(user);
+          return user;
+        })
+      );
+    }
+
+    logout() {
+      localStorage.removeItem('currentUser');
+      this.userSubject.next(new User());
+    }
+
+}
